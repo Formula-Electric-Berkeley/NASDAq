133a6dfacdb859b7fae1d704380c16369cc0d59b
Raadwan Masum <piraadwan@gmail.com>
2023-11-01 22:12:54 -0700

    fix flexbox to account for logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,14 +3,20 @@ import GraphLine from '../GraphLine/GraphLine';
 
 const Layout = () => {
     return (
-        <div className="flex justify-around w-full h-full">
-            <div className="p-5 w-1/2 h-1/2">
-                <GraphLine colorHex={"#0aefff"} />
-                <GraphLine colorHex={"#deff0a"} />
+        <div className="flex flex-col w-full h-screen">
+            <div className='flex justify-center p-10'>
+                <img width={200} src={"./FEBLOGO_NASDAQ.png"} alt="FEBLOGO_NASDAQ" />
             </div>
-            <div className="p-5 w-1/2 h-1/2">
-                <GraphLine colorHex={"#89fc00"} />
-                <GraphLine colorHex={"#ff0000"} />
+
+            <div className="flex justify-around w-full grow">
+                <div className="p-5 w-1/2 h-1/2">
+                    <GraphLine colorHex={"#0aefff"} />
+                    <GraphLine colorHex={"#deff0a"} />
+                </div>
+                <div className="p-5 w-1/2 h-1/2">
+                    <GraphLine colorHex={"#89fc00"} />
+                    <GraphLine colorHex={"#ff0000"} />
+                </div>
             </div>
         </div>
     );
