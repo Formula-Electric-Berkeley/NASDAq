3de790448a20fd6c12c596ca1d0ed988e3ed0e24
Raadwan Masum <piraadwan@gmail.com>
2023-11-01 21:41:53 -0700

    wrap GraphLine component in responsive container

--- src/components/GraphLine/GraphLine.js
+++ src/components/GraphLine/GraphLine.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 
-const GraphLine = ({colorHex}) => {
+const GraphLine = ({ colorHex }) => {
     let data = [{ timestamp: '0', value: 100 }, { timestamp: '1', value: 200 }, { timestamp: '2', value: 400 }, { timestamp: '3', value: 200 }, { timestamp: '4', value: 300 }, { timestamp: '5', value: 100 }, { timestamp: '6', value: 500 },];
-    
+
     return (
-        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-            <Line type="monotone" dataKey="value" stroke={colorHex} />
-            <XAxis dataKey="timestamp" />
-            <YAxis />
-            <Tooltip />
-        </LineChart>
+        <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="value" stroke={colorHex} />
+                <XAxis dataKey="timestamp" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+        </ResponsiveContainer>
     );
 };
 
