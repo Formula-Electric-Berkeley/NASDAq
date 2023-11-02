c292528f7ad8f171164d36382c1d6926b993ff52
Raadwan Masum <piraadwan@gmail.com>
2023-11-01 21:52:17 -0700

    fix layout flexbox

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,12 @@ import GraphLine from '../GraphLine/GraphLine';
 
 const Layout = () => {
     return (
-        <div className="flex w-full">
-            <div>
+        <div className="flex justify-around w-full h-full">
+            <div className="p-5 w-1/2 h-1/2">
                 <GraphLine colorHex={"#0aefff"} />
                 <GraphLine colorHex={"#deff0a"} />
             </div>
-            <div>
+            <div className="p-5 w-1/2 h-1/2">
                 <GraphLine colorHex={"#89fc00"} />
                 <GraphLine colorHex={"#ff0000"} />
             </div>
