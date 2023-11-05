1c29a0004e8699f1b93a8b81c0da8c5cb21a7fb8
Raadwan Masum <piraadwan@gmail.com>
2023-11-05 14:51:18 -0800

    format serial js

--- public/serial.js
+++ public/serial.js
@@ -4,24 +4,24 @@ const { ReadlineParser } = require('@serialport/parser-readline')
 var device;
 
 async function pollPorts() {
-  await SerialPort.list().then((ports, err) => {
-    for (i in ports) {
-      if (ports[i].manufacturer == "FTDI" && !device) {
-        device = new SerialPort({
-          path: ports[i].path,
-          baudRate: 9600,
-        })
+    await SerialPort.list().then((ports, err) => {
+        for (i in ports) {
+            if (ports[i].manufacturer == "FTDI" && !device) {
+                device = new SerialPort({
+                    path: ports[i].path,
+                    baudRate: 9600,
+                })
 
-        const parser = device.pipe(new ReadlineParser({ delimiter: '\n' }))
-        parser.on('data', console.log)
-      }
-    }
-  })
+                const parser = device.pipe(new ReadlineParser({ delimiter: '\n' }))
+                parser.on('data', console.log)
+            }
+        }
+    })
 }
 
 function connect() {
-  pollPorts();
-  setTimeout(connect, 2000);
+    pollPorts();
+    setTimeout(connect, 2000);
 }
 
 setTimeout(connect, 2000);
