8812ef57d9b3a504c86d382c8d5f8169d2463c7b
Raadwan Masum <piraadwan@gmail.com>
2023-11-05 13:00:32 -0800

    round serial monitor

--- src/components/Views/SerialMonitor/SerialMonitor.js
+++ src/components/Views/SerialMonitor/SerialMonitor.js
@@ -4,7 +4,7 @@ import './SerialMonitor.css'
 const SerialMonitor = () => {
     return (
         <div className='flex justify-center h-4/5'>
-            <div className='w-4/5 overflow-auto p-5 text-white bg-neutral-800 serial-monitor-div'>
+            <div className='w-4/5 overflow-auto p-5 text-white bg-neutral-800 serial-monitor-div rounded-xl'>
                 <h1>test</h1>
                 <h1>test</h1>
                 <h1>test</h1>
