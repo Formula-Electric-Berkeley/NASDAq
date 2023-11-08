ed4da999d0763603ce5d704fc3e2d4bb97c15bfc
Raadwan Masum <piraadwan@gmail.com>
2023-11-07 20:22:24 -0800

    add gauge component to heartbeat

--- src/components/Views/Heartbeat/Heartbeat.js
+++ src/components/Views/Heartbeat/Heartbeat.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import GraphLine from '../../Lib/GraphLine/GraphLine';
+import Gauge from '../../Lib/Gauge/Gauge'
 
 const Heartbeat = () => {
     return (
         <div className="flex justify-around w-full grow">
             <div className="p-5 w-1/2 h-1/2">
                 <GraphLine colorHex={"#0aefff"} />
-                <GraphLine colorHex={"#deff0a"} />
+                <GraphLine colorHex={"#ff0000"} />
             </div>
             <div className="p-5 w-1/2 h-1/2">
                 <GraphLine colorHex={"#89fc00"} />
-                <GraphLine colorHex={"#ff0000"} />
+                <div className='flex h-full items-center justify-center'>
+                    <Gauge percent={0.85} units={"MPH"} />
+                    <Gauge units={"°C"} />
+                </div>
+                {/* <GraphLine colorHex={"#deff0a"} /> */}
             </div>
         </div>
     );
