d77630699c46647b674978a30ab197eb947aa415
Raadwan Masum <piraadwan@gmail.com>
2023-11-09 17:37:07 -0800

    remove unused min max

--- src/components/Lib/GraphLine/GraphLine.js
+++ src/components/Lib/GraphLine/GraphLine.js
@@ -4,9 +4,6 @@ import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianG
 const GraphLine = ({ colorHex }) => {
     let data = [{ timestamp: '0', value: 100 }, { timestamp: '1', value: 200 }, { timestamp: '2', value: 400 }, { timestamp: '3', value: -200 }, { timestamp: '4', value: 300 }, { timestamp: '5', value: 100 }, { timestamp: '6', value: 500 },];
 
-    const minX = Math.min(...data.map((d) => d.x));
-    const minY = Math.min(...data.map((d) => d.y));
-
     return (
         <ResponsiveContainer width="100%" height="100%">
             <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
