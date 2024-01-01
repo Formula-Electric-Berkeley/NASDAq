0e25493a69977594e36d4b2a1e69c4daafe6bc0e
Raadwan Masum <piraadwan@gmail.com>
2023-12-31 22:13:27 -0800

    archive old layout

---
+++ src/components/Layout/LayoutOld.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Heartbeat from '../Views/Heartbeat/Heartbeat';
+import SerialMonitor from '../Views/SerialMonitor/SerialMonitor';
+
+const Layout = () => {
+    return (
+        <div className="flex flex-col w-full h-screen">
+            <div className='flex justify-center py-7'>
+                <img width={200} src={"./FEBLOGO_NASDAQ.png"} alt="FEBLOGO_NASDAQ" />
+            </div>
+
+            <Heartbeat/>
+            {/* <SerialMonitor/> */}
+        </div>
+    );
+};
+
+export default Layout;
