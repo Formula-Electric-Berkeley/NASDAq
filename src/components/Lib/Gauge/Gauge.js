8728494b1ccd37911d4dcb0ddcde6683ce1e6509
Raadwan Masum <piraadwan@gmail.com>
2024-01-01 19:50:34 -0800

    put gauge into demo mode

--- src/components/Lib/Gauge/Gauge.js
+++ src/components/Lib/Gauge/Gauge.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import GaugeComponent from 'react-gauge-component'
 
 const Gauge = ({ units, value }) => {
+    const [currentValueDemo, setCurrentValueDemo] = useState(50);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCurrentValueDemo(Math.random() * 100);
+            // setArcs([{ limit: 30 }, { limit: 35 }, { limit: 100 }])
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    });
+
     return (
         <div className='w-full h-full flex justify-center items-center'>
+            <GaugeComponent
+                id="gauge-component-radial3"
+                value={currentValueDemo}
+                type="grafana"
+                arc={{
+                    colorArray: ['#1EFF00', '#CE1F1F'],
+                    nbSubArcs: 80,
+                    padding: 0.02,
+                    width: 0.3
+                }}
+                labels={{
+                    valueLabel: { matchColorWithArc: true },
+                }}
+                pointer={{ animationDelay: 0 }}
+            />
+            {/*
             <GaugeComponent
                 arc={{
                     subArcs: [
@@ -31,6 +60,7 @@ const Gauge = ({ units, value }) => {
                 }}
                 value={value}
             />
+            */}
         </div>
     );
 };
