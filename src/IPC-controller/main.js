cdd74ec25fbfa5a780f398a318fba00e7c3ea0a4
Raadwan Masum <piraadwan@gmail.com>
2024-06-10 21:49:02 -0400

    add support for different messages

--- src/IPC-controller/main.js
+++ src/IPC-controller/main.js
@@ -8,11 +8,13 @@ const database = new sqlite3.Database('./public/db.sqlite3', (err) => {
 });
 
 const device = new SerialPort({
-    path: '/dev/tty.usbserial-A50285BI',
+    path: '/dev/tty.usbserial-AB0PUOGD',
     baudRate: 115200,
 })
 
 let testGraphData = [0,1,2,3,4,5,6,7,8,9]
+let testTempData = [0,4,2,5,4,6,6,10,8,9]
+let testVoltageData = [3,1,2,7,4,6,8,7,8,9]
 
 const parser = device.pipe(new ReadlineParser({ delimiter: '\n' }))
 parser.on('data', function(data) {
@@ -31,15 +33,28 @@ parser.on('data', function(data) {
 })
 
 ipcMain.on('asynchronous-message', (event, arg) => {
-    const sql = arg;
-    if (arg.substr(0, 3) === "sql") {
-        database.all(sql.slice(4), (err, rows) => {
-            event.reply('asynchronous-reply', (err && err.message) || rows);
-        });
-    } else {
-        event.reply('asynchronous-reply', testGraphData);
-        //console.log("arg", arg)
+    // const sql = arg;
+    // if (arg.substr(0, 3) === "sql") {
+    //     database.all(sql.slice(4), (err, rows) => {
+    //         event.reply('asynchronous-reply', (err && err.message) || rows);
+    //     });
+    // }
+
+    if (arg.substr(0, 4) === "PING") {
+        event.reply('asynchronous-reply', "PONG");
     }
+
+    if (arg.substr(0, 5) === "TDATA") {
+        event.reply('asynchronous-reply', testTempData);
+    }
+
+    if (arg.substr(0, 5) === "VDATA") {
+        event.reply('asynchronous-reply', testVoltageData);
+    }
+
+    // {
+    //     event.reply('asynchronous-reply', "INVALID REQUEST KEY");
+    // }
 });
 
 async function listSerialPorts() {
