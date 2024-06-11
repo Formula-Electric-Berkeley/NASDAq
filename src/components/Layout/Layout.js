055f70353a73538da045a49edc31a868f07004bb
Raadwan Masum <piraadwan@gmail.com>
2024-06-10 21:49:26 -0400

    change default layout to only show two pieces of data

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -33,6 +33,112 @@ var json = {
                     "component": "SerialMonitor",
                     "enableClose": false,
                     "icon": "icons/terminal.svg"
+                }
+            ]
+        }
+    ],
+    "layout": {
+        "type": "row",
+        "id": "#8e1badf6-eca0-4bda-95ea-21fdced44190",
+        "children": [
+            {
+                "type": "row",
+                "id": "#41619668-859e-410f-89b5-6609cfb91ccf",
+                "weight": 58.57358907266485,
+                "children": [
+                    {
+                        "type": "tabset",
+                        "id": "#7e54492e-9da9-436d-8277-f49595ca1be0",
+                        "weight": 27.653301886792452,
+                        "children": [
+                            {
+                                "type": "tab",
+                                "id": "#b9273023-2061-43c4-a1d0-67f7880e1c85",
+                                "name": "Temperature",
+                                "component": "GraphTemp"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "tabset",
+                        "id": "#6797bff9-9b04-4ee7-95c2-0f68f350b480",
+                        "weight": 27.653301886792452,
+                        "children": [
+                            {
+                                "type": "tab",
+                                "id": "#2217c858-0a5a-498b-bf89-0aaaf96ad682",
+                                "name": "Voltate",
+                                "component": "GraphVoltage"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "type": "row",
+                "id": "#7a64fa52-7d6a-48a0-8369-d4c412a4ed36",
+                "weight": 16.595850237255043,
+                "children": [
+                    {
+                        "type": "tabset",
+                        "id": "#58882a52-1a25-49fa-8d34-f1678260158f",
+                        "weight": 17.976190476190474,
+                        "children": [
+                            {
+                                "type": "tab",
+                                "id": "#6b0834e3-2c16-411a-ba99-dbd81b1372b8",
+                                "name": "Temperature",
+                                "component": "Gauge"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "tabset",
+                        "id": "#94694a20-3892-42d8-ae66-fb060f7183c0",
+                        "weight": 17.976190476190474,
+                        "children": [
+                            {
+                                "type": "tab",
+                                "id": "#92fb98ce-f167-407a-b1ca-f2dc4ff50da0",
+                                "name": "Voltage",
+                                "component": "Gauge"
+                            }
+                        ],
+                        "active": true
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+var json_old = {
+    "global": {
+        "tabEnableFloat": true,
+        "tabSetMinWidth": 100,
+        "tabSetMinHeight": 100,
+        "borderMinSize": 100
+    },
+    "borders": [
+        {
+            "type": "border",
+            "location": "bottom",
+            "children": [
+                {
+                    "type": "tab",
+                    "id": "#9294e836-a731-4b5c-af6b-fc846f9746ce",
+                    "name": "CONNECT STATUS",
+                    "component": "grid",
+                    "enableClose": false,
+                    "icon": "icons/signal.svg"
+                },
+                {
+                    "type": "tab",
+                    "id": "#47dcf2fe-a1ca-43f9-b062-afdbdb8390de",
+                    "name": "SERIAL MONITOR",
+                    "component": "SerialMonitor",
+                    "enableClose": false,
+                    "icon": "icons/terminal.svg"
                 },
             ]
         }
@@ -55,7 +161,7 @@ var json = {
                                 "type": "tab",
                                 "id": "#b9273023-2061-43c4-a1d0-67f7880e1c85",
                                 "name": "----------",
-                                "component": "Graph"
+                                "component": "GraphTemp"
                             }
                         ]
                     },
@@ -73,7 +179,7 @@ var json = {
                                         "type": "tab",
                                         "id": "#2217c858-0a5a-498b-bf89-0aaaf96ad682",
                                         "name": "----------",
-                                        "component": "Graph"
+                                        "component": "GraphVoltage"
                                     }
                                 ]
                             },
@@ -173,7 +279,7 @@ const Landing = () => {
     }
 
     const factory = (node) => {
-        //console.log(model.toJson());
+        // console.log(model.toJson());
         var component = node.getComponent();
 
         if (component === "SerialMonitor") {
@@ -185,7 +291,15 @@ const Landing = () => {
         }
 
         if (component === "Graph") {
-            return <GraphLine colorHex={"#0aefff"} />;
+            return <GraphLine colorHex={"#eb9550"} />;
+        }
+
+        if (component === "GraphTemp") {
+            return <GraphLine colorHex={"#fed100"} dataSource={"TEMP"} />;
+        }
+
+        if (component === "GraphVoltage") {
+            return <GraphLine colorHex={"#0aefff"} dataSource={"VOLTAGE"} />;
         }
 
         if (component === "Gauge") {
