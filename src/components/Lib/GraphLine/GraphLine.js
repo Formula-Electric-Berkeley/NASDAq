8758cceca4bfd7f75d18756a86686aaad4ea85de
Raadwan Masum <piraadwan@gmail.com>
2024-06-10 21:49:36 -0400

    add support for temp and voltage to graph

--- src/components/Lib/GraphLine/GraphLine.js
+++ src/components/Lib/GraphLine/GraphLine.js
@@ -7,14 +7,60 @@ import sendAsync from '../../../IPC-controller/renderer';
 
 const { app } = window.require('@electron/remote');
 
+const GraphLine = ({colorHex, dataSource}) => {
+    const [graphData, setGraphData] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: 'Live Data',
+                borderColor: colorHex,
+                data: [],
+            },
+        ],
+    });
 
-const GraphLine = () => {
+    useEffect(() => {
+        const intervalID = setInterval(() =>  {
+            if (dataSource === "TEMP") {
+                sendAsync('TDATA').then((result) => 
+                    {
+                    console.log("setting TEMP for " + dataSource + " with data " + result);
+                        setGraphData({
+                    labels: result,
+                    datasets: [
+                        {
+                            label: 'Live Temperature Data',
+                            borderColor: colorHex,
+                            data: result,
+                        },
+                    ],
+                })});
+            } else if (dataSource === "VOLTAGE") {
+                sendAsync('VDATA').then((result) => {
+                    console.log("setting VOLTAGE for " + dataSource + " with data " + result);
+                    setGraphData({
+                    labels: result,
+                    datasets: [
+                        {
+                            label: 'Live Voltage Data',
+                            borderColor: colorHex,
+                            data: result,
+                        },
+                    ],
+                })});
+            }
+        }, 250);
+        
+        return () => clearInterval(intervalID);
+    });
+
     const [data, setData] = useState({
         labels: [],
         datasets: [
             {
                 label: 'Live Data',
-                borderColor: 'rgb(75, 192, 192)',
+                // borderColor: 'rgb(75, 192, 192)',
+                borderColor: colorHex,
                 data: [],
             },
         ],
@@ -29,38 +75,13 @@ const GraphLine = () => {
         },
     };
 
-    useEffect(() => {
-        sendAsync('reqData').then((result) => 
-            setData((prevData) => {
-                const newDataPoints = result;
-
-                return {
-                    labels: [0,1,2,3,4,5,6,7,8,9],
-                    datasets: [
-                        {
-                            label: 'Live Data',
-                            borderColor: 'rgb(75, 192, 192)',
-                            data: newDataPoints,
-                        },
-                    ],
-                };
-            })
-        );
-    });
-
 //    useEffect(() => {
-//        const maxDataPoints = 20;
-//
-//        const interval = setInterval(() => {
-//            const newLabel = new Date().toLocaleTimeString();
-//            const newData = Math.floor(Math.random() * 100);
-//
+//        sendAsync('reqData').then((result) => 
 //            setData((prevData) => {
-//                const newLabels = [...prevData.labels, newLabel].slice(-maxDataPoints);
-//                const newDataPoints = [...prevData.datasets[0].data, newData].slice(-maxDataPoints);
+//                const newDataPoints = result;
 //
 //                return {
-//                    labels: newLabels,
+//                    labels: [0,1,2,3,4,5,6,7,8,9],
 //                    datasets: [
 //                        {
 //                            label: 'Live Data',
@@ -69,15 +90,42 @@ const GraphLine = () => {
 //                        },
 //                    ],
 //                };
-//            });
-//        }, 500);
-//
-//        return () => clearInterval(interval);
-//    }, []);
+//            })
+//        );
+//    });
+
+    useEffect(() => {
+        const maxDataPoints = 20;
+
+        const interval = setInterval(() => {
+            const newLabel = new Date().toLocaleTimeString();
+            const newData = Math.floor(Math.random() * 100);
+
+            setData((prevData) => {
+                const newLabels = [...prevData.labels, newLabel].slice(-maxDataPoints);
+                const newDataPoints = [...prevData.datasets[0].data, newData].slice(-maxDataPoints);
+
+                return {
+                    labels: newLabels,
+                    datasets: [
+                        {
+                            label: 'Live Data',
+                            //borderColor: '#fed100',
+                            borderColor: colorHex,
+                            //borderColor: 'rgb(75, 192, 192)',
+                            data: newDataPoints,
+                        },
+                    ],
+                };
+            });
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className='w-full h-full p-5'>
-            <Line data={data} options={options} />
+            <Line data={graphData} options={options} />
         </div>
     );
 };
